c0a4ba25a42ec9a1973593f86f5efec9be894aa4
erhanefebilici <swoord5085@gmail.com>
2024-12-21 17:06:39 +0300

    Diziler Array

---
+++ ders3.js
@@ -0,0 +1,35 @@
+// Dizi (Array) birden fazla değer saklar ve sıralıdır.
+
+var notlar = [30, 70, 80, 55]
+var ogrenciler = ["Batuhan", "Erhan", "Batu", "Adem"]
+
+// console.log(ogrenciler); // tümünü yazdırmak için.
+// console.log(ogrenciler[3]); // tek bir elemanı yazdırmak için.
+// köşeli parantez [] içine yazılan sayıya sıra numarası index denir.
+// DIKKAT: 0'dan başlar.
+
+// Eleman Ekleme
+// console.log(ogrenciler);
+// ogrenciler.push("Ahmet")
+// console.log(ogrenciler);
+
+// Eleman Silme
+// console.log(ogrenciler);
+// ogrenciler.pop() // sadece son elemanı siler
+// console.log(ogrenciler);
+
+// Bir Elemanın index Numarısını Verme
+//var silinecekElemanIndex = ogrenciler.indexOf("Erhan")
+// console.log(silinecekElemanIndex);
+//ogrenciler.splice(silinecekElemanIndex, 1)
+// belirtilen sayıdan başlayıp ikinci belirtilen sayı kadar eleman siler.
+//console.log(ogrenciler);
+
+// Sıralama
+//ogrenciler.sort() // büyükten küçüğe ve A'dan Z'ye sıralar.
+//ogrenciler.reverse() // küçükten büyüğe ve Z'den A'ya sıralar.
+//console.log(ogrenciler);
+
+// Uzunluk (Eleman Sayısı Öğrenme)
+// console.log(ogrenciler.length);
+// console.log("Batuhan".length); // Batuhan kelimesinin uzunluğu 7 harftir.
