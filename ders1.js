67fe9a6b9c7fd030dc77039e9fbd4b176c291c02
erhanefebilici <swoord5085@gmail.com>
2024-12-21 16:56:42 +0300

    Ekrana Çıktı verme, değişkenler ve Temel Veri tipleri

---
+++ ders1.js
@@ -0,0 +1,43 @@
+// Ekrana yorum yazdırma fonksiyonları
+// alert("merhaba dunya");
+// console.log("merhaba dunya 2");
+// document.write("hello 3");
+//---------------------------------------------
+// veri = data
+// var = variable
+// ş ö ü İ ç ı ğ harflerini kesinlikle kullanma
+var degisken = "Ritim";
+console.log(degisken);
+
+degisken = "Batu - Erhan";
+console.log(degisken);
+
+var sayi1 = 50;
+var sayi2 = 13;
+
+console.log(sayi1 + sayi2);
+console.log(sayi1 * sayi2);
+console.log(sayi1 - sayi2);
+console.log(sayi1 / sayi2);
+
+//temel öperatorler
+
+console.log(5 ** 3);
+console.log(53 % 2);
+console.log(++sayi1);
+console.log(--sayi1);
+
+// veri tipleri (ilkel)
+
+var d1 = 20; // tam sayı (intenger)
+var d2 = 3.14; // ondalık sayı (float)
+var d3 = "hello"; // dize metni (string)
+
+// typeof değişken tipini verir
+
+console.log(typeof d3);
+// ÖDEV
+// Kendinizle ilgili bilgileri konsol ekranına yazdıran porgram
+// Adınız, soyadınız, hobileriniz, doğum tarihiniz, yaşınız
+// Yeni bir javascript dosyası oluştur ve ismi "odev1" olsun
+// HTML'e bağlamayı unutma "<script></script>"
