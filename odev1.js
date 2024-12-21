532ebed60eed433c2b922a2ccb7103137ab7a7ed
erhanefebilici <swoord5085@gmail.com>
2024-12-21 17:00:21 +0300

    Ders Ortalama Hesaplama

---
+++ odev1.js
@@ -0,0 +1,17 @@
+var tanitim = "Merhaba ben Erhan Efe Bilici"
+console.log(tanitim);
+
+var okulum = "FSM Anadolu Lisesin'de okuyorum"
+console.log(okulum);
+
+var dersim = "11. Sınıfım ve dil dersi görüyorum"
+console.log(dersim);
+
+var favoridersim = "En sevdiğim ders İngilizce"
+console.log(favoridersim);
+
+var hobilerim = "Hobilerim müzik yapmak, video oyunu oynamak, arkadaşlarla dışarı çıkıp gezmek"
+console.log(hobilerim);
+
+var sevdiklerim = "Boş zamanlarımda resim yapmayı, arkadaşlarımla sohbet etmeyi, bisiklet sürmeyi severim"
+console.log(sevdiklerim);
